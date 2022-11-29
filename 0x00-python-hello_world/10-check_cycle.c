f6a32bd529f20478eafdbbde098c10480a71e7f5
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-11-29 23:45:06 +0100

    Create check_cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * check_cycle -  checks if linked list is a cycle
+ * @list: pointer to head of the list 
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */ 
+int check_cycle(listint_t *list)
+{
+	listint_t *turtle, *hare;
+
+	if (list == NULL || list->next == NULL)
+		return (0);
+
+	turtle = list->next;
+	hare = list->next->next;
+
+	while (turtle && hare && hare->next)
+	{
+		if (turtle == hare)
+			return (1);
+
+		turtle = turtle->next;
+		hare = hare->next->next;
+	}
+
+	return (0);
+}
