5a8f786c79e45155990ad42019673549831a3e2e
Sulaiman Aminu Barkindo <sulaimanaminu02@gmail.com>
2022-12-02 22:33:58 +0100

    Correct task 1 lesson2

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python3
 # 1-calculation.py
-# Brennan D Baraban <375@holbertonschool.com>
 
 if __name__ == "__main__":
     """Print the sum, difference, multiple and quotient of 10 and 5."""
@@ -9,7 +8,7 @@ if __name__ == "__main__":
     a = 10
     b = 5
 
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
+    print(f"{a} + {b} = {add(a,b)}")
+    print(f"{a} - {b} = {sub(a,b)}")
+    print(f"{a} * {b} = {mul(a,b)}")
+    print(f"{a} / {b} = {div(a,b)}")
